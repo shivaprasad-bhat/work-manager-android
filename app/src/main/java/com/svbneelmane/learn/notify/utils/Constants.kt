70df27cb2dde2db2e9ec5e87d8c51617b1995de0
Shivaprasad Bhat <svbhat.neelmane@gmail.com>
2021-06-15 00:13:20 +0530

    Adding Constants.kt
    Adding project level constant file

---
+++ app/src/main/java/com/svbneelmane/learn/notify/utils/Constants.kt
@@ -0,0 +1,17 @@
+package com.svbneelmane.learn.notify.utils
+
+/**
+ * Constants - Project Level
+ */
+class Constants {
+    companion object {
+        const val CHANNEL_NAME = "Default"
+        const val CHANNEL_ID = "1234"
+        const val CHANNEL_DESCRIPTION = "Default Channel"
+
+        const val NOTIFICATION_TITLE = "Knock Knock, User.."
+        const val NOTIFICATION_CONTENT_TEXT =
+            "You've a notification, this will be sent every 15 minutes. If you are getting this periodically, inform developer @svbneelmane"
+        const val NOTIFICATION_ID = 1
+    }
+}
